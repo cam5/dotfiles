85f1a78dd3b7b60df62d244819959e27ec6682a8
Cameron Hurd <me@cameronhurd.com>
2016-11-10 14:20:13 -0500

    make MAMP great again. #MAMPA

--- config/env.sh
+++ config/env.sh
@@ -1,15 +1,16 @@
 #!/bin/zsh
 
 export EDITOR='nvim'
+export PHP_VERSION='php7.0.0'
 
 # PATH
 export BREW="/usr/local/bin:/usr/local/sbin"
 export SYSTEM="/usr/bin:/bin:/usr/sbin:/sbin"
 export X11="/usr/X11/bin"
 export RUBY="~/.rvm/bin:~/.rvm/gems"
-export MAMP="/Applications/MAMP/bin/php/php5.6.2/bin/php"
+export MAMP="/Applications/MAMP/bin/php/${PHP_VERSION}/bin"
 
-export PATH="$BREW:$RUBY:$SYSTEM:$X11"
+export PATH="$MAMP:$BREW:$RUBY:$SYSTEM:$X11"
 
 # Owner
 export USER_NAME="dev"
@@ -29,3 +30,7 @@ alias gd='git diff'
 
 # PHP Ctag helper
 alias phpctags='ctags -R --fields=+aimS --languages=php'
+
+if [[ $hostname == 'cambook' ]]; then
+    alias wp="$MAMP/wp-cli.phar"
+fi
